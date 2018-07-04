58249c3e551c5bfa2e1a21e00cb80557614be445
xp-zhao <1132813751@qq.com>
2018-07-04 20:36:18 +0800

    crontab 表达式转换

--- learn-test/src/main/java/com/xp/quartz/Test.java
+++ learn-test/src/main/java/com/xp/quartz/Test.java
@@ -1,6 +1,9 @@
 package com.xp.quartz;
 
+import com.cronutils.mapper.CronMapper;
 import com.cronutils.model.Cron;
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinition;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.parser.CronParser;
 import org.quartz.CronExpression;
@@ -17,22 +20,54 @@ public class Test
 	{
 		Date curTime = new Date();
 		System.out.println(curTime);
-
-//		CronExpression expression;
-//		try
-//		{
-////			expression = new CronExpression("0/5 * * * * ?");
-//			expression = new CronExpression("0 0/1 * * * ?");
-//			Date newDate = expression.getNextValidTimeAfter(curTime);
-//			System.out.println(newDate);
-//		} catch (ParseException e) {
-//			System.out.println("fail to parse cron express");
-//		} catch (Exception e) {
-//			System.out.println("fail to update rule nextTime");
-//		}
+		String cron = "0/5 * * * * ?";
+		CronExpression expression;
+		try
+		{
+			expression = new CronExpression(cron);
+			Date newDate = expression.getNextValidTimeAfter(curTime);
+			System.out.println(newDate);
+		} catch (ParseException e) {
+			System.out.println("fail to parse cron express");
+		} catch (Exception e) {
+			System.out.println("fail to update rule nextTime");
+		}
 //		cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(QUARTZ);
 //
 //		CronParser parser = new CronParser(cronDefinition);
 //		Cron quartzCron = parser.parse("0 23 * ? * 1-5 *");
+	}
+
+	public static void test()
+	{
+		CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ);
+		CronParser parser = new CronParser(cronDefinition);
+		Cron quartzCron = parser.parse("0 23 * ? * 1-5 *");
+//		CronMapper cronMapper = CronMapper.fromQuartzToCron4j();
+		CronMapper cronMapper = CronMapper.fromQuartzToUnix();
+		Cron cron4jCron = cronMapper.map(quartzCron);
+		System.out.println(cron4jCron.asString());//will return: 23 * * * 1-5
+	}
+
+	public static void test2()
+	{
+		CronDefinition cronDefinition = CronDefinitionBuilder.instanceDefinitionFor(CronType.UNIX);
+		CronParser parser = new CronParser(cronDefinition);
+		Cron quartzCron = parser.parse("0/5 * * * * ?");
+		CronMapper cronMapper = CronMapper.fromUnixToQuartz();
+		Cron cron4jCron = cronMapper.map(quartzCron);
+		String cron = cron4jCron.asString();
+		System.out.println(cron);
+		CronExpression expression;
+		try
+		{
+			expression = new CronExpression(cron);
+			Date newDate = expression.getNextValidTimeAfter(new Date());
+			System.out.println(newDate);
+		} catch (ParseException e) {
+			System.out.println("fail to parse cron express");
+		} catch (Exception e) {
+			System.out.println("fail to update rule nextTime");
+		}
 	}
 }
