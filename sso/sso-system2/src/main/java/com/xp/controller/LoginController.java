d0b3226a0e5272a7b850ec52043eea30ace0ac67
xp-zhao <13688396271@163.com>
2018-07-09 18:01:24 +0800

    sso web-system

---
+++ sso/sso-system2/src/main/java/com/xp/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.xp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xp-zhao on 2018/7/9.
+ */
+@Controller
+public class LoginController
+{
+	@RequestMapping (value = "/hello/helloWorld.do",method= RequestMethod.GET)
+	public String helloWorld(HttpServletRequest request,HttpServletResponse response,Model model) throws IOException
+	{
+		String sessionId = request.getSession().getId();
+		System.out.println("sessionId: "+sessionId);
+		return "index";
+	}
+}
