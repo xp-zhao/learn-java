f127efd1536630882b4c28ec535c9fa15f9924a1
xp-zhao <1132813751@qq.com>
2018-07-11 00:13:42 +0800

    sso login

--- sso/sso-system1/src/main/java/com/xp/controller/LoginController.java
+++ sso/sso-system1/src/main/java/com/xp/controller/LoginController.java
@@ -1,27 +1,48 @@
 package com.xp.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.xp.utils.RedisUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
  * Created by xp-zhao on 2018/7/9.
  */
 @Controller
+@RequestMapping("/sso")
 public class LoginController
 {
-	@RequestMapping (value = "/hello/helloWorld.do",method= RequestMethod.GET)
-	public String helloWorld(HttpServletRequest request,HttpServletResponse response,Model model) throws IOException
+	@RequestMapping (value = "/login.do",method= RequestMethod.GET)
+	@ResponseBody
+	public Object helloWorld(String username, String password,HttpServletRequest request,HttpSession session,HttpServletResponse response,Model model) throws IOException
 	{
+		JSONObject result = new JSONObject();
 		String sessionId = request.getSession().getId();
-		RedisUtil.set("system1-sessionId:" , sessionId);
+		String sessionId1 = session.getId();
+		if(StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+			result.put("code" , 0);
+			result.put("msg" , "用户名密码不能为空");
+			return result;
+		}
+		String lognUser = (String) RedisUtil.get(sessionId);
+		if(StringUtils.isNoneBlank(lognUser)){
+			result.put("code" , 1);
+			result.put("msg" , "当前登录用户：" + username);
+			return result;
+		}
+		RedisUtil.set(sessionId , username);
 		System.out.println("sessionId: "+sessionId);
-		return "index";
+		result.put("code" , 1);
+		result.put("msg" , "登录成功");
+		return result;
 	}
 }
