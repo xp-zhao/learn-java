eb4593b2c28212728a3c28465f78c678b46d956c
xp-zhao <1132813751@qq.com>
2018-09-30 22:21:59 +0800

    '工厂模式'

---
+++ HeadFirst/src/main/java/com/xp/AbstractProduct/ConcreteFactory1.java
@@ -0,0 +1,17 @@
+package com.xp.AbstractProduct;
+
+/**
+ * Created by xp-zhao on 2018/9/30.
+ */
+public class ConcreteFactory1 extends AbstractFactory
+{
+	AbstractProductA createProductA()
+	{
+		return new ProductA1();
+	}
+
+	AbstractProductB createProductB()
+	{
+		return new ProductB1();
+	}
+}
---
+++ HeadFirst/src/main/java/com/xp/SimpleFactroy/Client.java
@@ -0,0 +1,13 @@
+package com.xp.SimpleFactroy;
+
+/**
+ * Created by xp-zhao on 2018/9/30.
+ */
+public class Client
+{
+	public static void main(String[] args)
+	{
+		SimpleFactory factory = new SimpleFactory();
+		Product product = factory.createProduct(1);
+	}
+}
