40b5f1e56cda7e90dd00e8be62498cfc3bb0f0f1
xp-zhao <1132813751@qq.com>
2018-10-02 23:36:34 +0800

    'litespring'

--- learn-spring/litespring/src/main/java/org/litespring/beans/BeanDefinition.java
+++ learn-spring/litespring/src/main/java/org/litespring/beans/BeanDefinition.java
@@ -7,16 +7,16 @@ import java.util.List;
  */
 public interface BeanDefinition
 {
-	public static final String SCOPE_SINGLETON = "singleton";
-	public static final String SCOPE_PROTOTYPE = "prototype";
-	public static final String SCOPE_DEFAULT = "";
+	String SCOPE_SINGLETON = "singleton";
+	String SCOPE_PROTOTYPE = "prototype";
+	String SCOPE_DEFAULT = "";
 
-	public boolean isSingleton();
-	public boolean isPrototype();
+	boolean isSingleton();
+	boolean isPrototype();
 	String getScope();
 	void setScope(String scope);
 
-	public String getBeanClassName();
+	String getBeanClassName();
 
 	List<PropertyValue> getPropertyValues();
 }
--- learn-spring/litespring/src/main/java/org/litespring/beans/factory/config/ConfigurableBeanFactory.java
+++ learn-spring/litespring/src/main/java/org/litespring/beans/factory/config/ConfigurableBeanFactory.java
@@ -3,6 +3,7 @@ package org.litespring.beans.factory.config;
 import org.litespring.beans.factory.BeanFactory;
 
 /**
+ * 提供设置 classloader 的能力
  * Created by xp-zhao on 2018/7/24.
  */
 public interface ConfigurableBeanFactory extends BeanFactory
