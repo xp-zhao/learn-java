e116826b5ebdf8c5a83c43dab0a5712a3ac59662
xp-zhao <13688396271@163.com>
2019-01-11 17:51:06 +0800

    ''

---
+++ learn-lambda/src/main/java/com/xp/file/FileModel.java
@@ -0,0 +1,18 @@
+package com.xp.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by xp-zhao on 2019/1/11.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileModel
+{
+	private String url;
+	private String date;
+	private String phone;
+}
