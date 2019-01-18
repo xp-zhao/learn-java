55ee0f82f27443192fe8c64900c4943a7b798634
xp-zhao <13688396271@163.com>
2019-01-18 16:11:12 +0800

    'LeetCode_39_组合总和'

---
+++ leetcode/src/main/java/array/LeetCode_39_CombinationSum.java
@@ -0,0 +1,68 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ *
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ candidates 中的数字可以无限制重复被选取。
+
+ 说明：
+ 所有数字（包括 target）都是正整数。
+ 解集不能包含重复的组合。
+
+ 示例 1:
+ 输入: candidates = [2,3,6,7], target = 7,
+ 所求解集为:
+ [
+ [7],
+ [2,2,3]
+ ]
+
+ 示例 2:
+ 输入: candidates = [2,3,5], target = 8,
+ 所求解集为:
+ [
+ [2,2,2,2],
+ [2,3,3],
+ [3,5]
+ ]
+ *
+ * Created by xp-zhao on 2019/1/18.
+ */
+public class LeetCode_39_CombinationSum
+{
+	public static void main(String[] args) {
+		int[] candidates = {2 , 3 , 5};
+		int target = 8;
+		System.out.println(combinationSum(candidates,target));
+	}
+
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> result = new ArrayList<>();
+		if(candidates.length == 0){
+			return result;
+		}
+		dfs(result,new ArrayList<>(),candidates,target,0);
+		return result;
+	}
+
+	public static void dfs(List<List<Integer>> result, List<Integer> list, int[] candidates, int target, int index){
+		int sum = list.stream().reduce(0,Integer::sum);
+		if(sum >= target){
+			if(sum == target){
+				result.add(new ArrayList<>(list));
+			}
+			return;
+		}else{
+			for(int i = index; i < candidates.length; i++)
+			{
+				list.add(candidates[i]);
+				dfs(result, list, candidates, target, i);
+				list.remove(list.size() - 1);
+			}
+		}
+	}
+}
