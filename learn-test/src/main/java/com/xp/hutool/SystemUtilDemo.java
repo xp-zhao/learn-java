cf46a6191d6f2066f99c5e93ba72d53f1f96138a
xp-zhao <13688396271@163.com>
2019-05-17 09:20:45 +0800

    引入hutool工具包

---
+++ learn-test/src/main/java/com/xp/hutool/SystemUtilDemo.java
@@ -0,0 +1,15 @@
+package com.xp.hutool;
+
+import cn.hutool.system.SystemUtil;
+
+/**
+ * @description: hutool工具包系统属性调用
+ * @author: zhaoxp
+ * @create: 2019/05/16
+ **/
+public class SystemUtilDemo {
+
+  public static void main(String[] args) {
+    System.out.println(SystemUtil.getRuntimeInfo());
+  }
+}
