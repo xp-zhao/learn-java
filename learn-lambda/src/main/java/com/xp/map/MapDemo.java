aca21ea8e398b3c8f26658b736b6a0d657171510
xp-zhao <13688396271@163.com>
2019-07-09 15:56:43 +0800

    map与flatmap使用示例

--- learn-lambda/src/main/java/com/xp/map/MapDemo.java
+++ learn-lambda/src/main/java/com/xp/map/MapDemo.java
@@ -1,20 +1,38 @@
 package com.xp.map;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by xp-zhao on 2018/12/28.
  */
-public class MapDemo
-{
-	public static void main(String[] args){
-		Map<String, Integer> map = new HashMap<>();
-		map.put("1" , 1);
-		map.put("2" , 2);
-		map.put("3" , 3);
-		map.put("4" , 4);
-		map.put("5" , 5);
-		map.forEach((k,v) -> System.out.println(k + "=" + v));
-	}
+public class MapDemo {
+
+  public static void main(String[] args) {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("1", 1);
+    map.put("2", 2);
+    map.put("3", 3);
+    map.put("4", 4);
+    map.put("5", 5);
+    map.forEach((k, v) -> System.out.println(k + "=" + v));
+
+    List<String> words = Arrays.asList("Java 8", "Lambdas", "In", "Action");
+    // 显示单词有几个字母
+    words.stream()
+        .map(String::length)
+        .collect(Collectors.toList())
+        .forEach(System.out::println);
+    // 显示单词表里面的不同字符
+    List<String> chars = words.stream()
+        .map(s -> s.split(""))
+        .flatMap(Arrays::stream)
+        .distinct()
+        .collect(Collectors.toList());
+    System.out.println(chars);
+  }
 }
