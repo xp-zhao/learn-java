9eeeb7ffe189f3db3ce8ecd1553b505e4500c464
xp-zhao <13688396271@163.com>
2019-08-14 20:06:13 +0800

    字符串示例

---
+++ learn-lambda/src/main/java/com/xp/stream/Demo.java
@@ -0,0 +1,30 @@
+package com.xp.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Demo.java
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/08/09
+ **/
+public class Demo {
+
+  public static void main(String[] args) {
+    List<Integer> ids = Arrays.asList(1, 2, 3, 4);
+    String str = ids.stream()
+        .map(item -> String.valueOf(item))
+        .collect(Collectors.joining(","));
+    System.out.println(str);
+
+    String nodeId = "RJB_SX_KBZSD_1";
+    System.out.println(nodeId.hashCode());
+    System.out.println(nodeId.split("_")[1]);
+
+    for (String s : "".split(":;")) {
+      System.out.println("s:"+s);
+    }
+  }
+}
--- learn-test/src/main/java/com/xp/basics/string/SwithDemo.java
+++ learn-test/src/main/java/com/xp/basics/string/SwithDemo.java
@@ -1,5 +1,12 @@
 package com.xp.basics.string;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * SwithDemo.java
  *
@@ -9,14 +16,25 @@ package com.xp.basics.string;
 public class SwithDemo {
 
   public static void main(String[] args) {
+
+    List<String> property = Arrays
+        .asList("n.node_id", "n.name", "n.knowledge_type", "n.important", "n.difficut", "n.electives",
+            "n.ability", "n.require", "n.require1", "n.require2", "n.require3", "n.BSDB_is_meta", "n.RJB_is_meta",
+            "n.point1", "n.point2", "n.point3", "n.point4");
+    String str = property.stream().collect(Collectors.joining(","));
+    String cql = "match (n) where id(n) = %s remove %s";
+    System.out.println(String.format(cql, 11, str));
+
+    Map<String, Object> map = new HashMap<>();
+    System.out.println(Optional.ofNullable(map.get("key")).orElse(""));
     String[] point = {"1", "2", "3"};
     Demo demo = new Demo();
     setPoint(demo, point);
     System.out.println(demo);
   }
 
-  public static void setPoint(Demo demo, String[] point){
-    switch (point.length){
+  public static void setPoint(Demo demo, String[] point) {
+    switch (point.length) {
       case 4:
         demo.setPoint4(point[3]);
       case 3:
