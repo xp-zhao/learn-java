5012c395f163c96eff1c2b6fde7ce76dd707eb12
xp-zhao <13688396271@163.com>
2019-08-20 11:55:53 +0800

    使用小顶堆找到两个大文件中重复数的前100个数

--- learn-algorithm/src/main/java/bigdata/Example1.java
+++ learn-algorithm/src/main/java/bigdata/Example1.java
@@ -9,13 +9,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- * Example1.java 示例 有2个txt文件，每个文件包含2.5亿个随机的不重复正整数，每个整数占据一行，内存限制为4M，请编写程序实现如下功能： 1.获取这两个文件中重复的正整数；
- * 2.将获取重复的数值里按从大到小排序的前100个数，确保不会出现内存溢出（有可能重复的正整数大小会超过4M） 3.返回的数值形成字符串，用英文,隔开
+ * Example1.java 示例 有2个txt文件，每个文件包含2.5亿个随机的不重复正整数，每个整数占据一行，内存限制为4M，请编写程序实现如下功能：
+ * 1.获取这两个文件中重复的正整数；
+ * 2.将获取重复的数值里按从大到小排序的前100个数，确保不会出现内存溢出（有可能重复的正整数大小会超过4M）
+ * 3.返回的数值形成字符串，用英文,隔开
  *
  * @author: zhaoxiaoping
  * @date: 2019/08/19
@@ -29,18 +33,21 @@ public class Example1 {
 //    cutFile("D:\\user\\code\\file\\bigdata\\file1.txt", "D:\\user\\code\\file\\bigdata\\file1");
 //    cutFile("D:\\user\\code\\file\\bigdata\\file2.txt", "D:\\user\\code\\file\\bigdata\\file2");
     // 找到重复的数据，保存到 repeat.txt 中
-    findRepeat("D:\\user\\code\\file\\bigdata\\repeat.txt");
+//    findRepeat("D:\\user\\code\\file\\bigdata\\repeat.txt");
     // 构造堆
     Heap heap = new Heap(100);
     File file = new File("D:\\user\\code\\file\\bigdata\\repeat.txt");
     try (BufferedReader reader = new BufferedReader(
         new InputStreamReader(new FileInputStream(file)))) {
       String line;
-      while ((line = reader.readLine()) != null){
+      while ((line = reader.readLine()) != null) {
         heap.insert(Integer.valueOf(line));
       }
     } catch (IOException e) {
     }
+    String str = Arrays.stream(heap.arr).filter(item -> item > 0)
+        .mapToObj(item -> String.valueOf(item)).collect(Collectors.joining(","));
+    System.out.println(str);
   }
 
   /**
@@ -126,18 +133,66 @@ public class Example1 {
     }
   }
 
+  /**
+   * 小顶堆
+   */
   static class Heap {
 
     private int[] arr;
+    private int n;
     private int count;
 
     public Heap(int capacity) {
-      arr = new int[capacity];
+      // 从下标 1 开始存储数据
+      arr = new int[capacity + 1];
+      n = capacity;
       count = 0;
     }
 
     public void insert(int data) {
-      arr[count++] = data;
+      if (count < n) {
+        count++;
+        arr[count] = data;
+        int i = count;
+        // 从下往上堆化
+        while (i / 2 > 0 && arr[i] < arr[i / 2]) {
+          swap(arr, i, i / 2);
+          i = i / 2;
+        }
+        return;
+      }
+      if (data < arr[1]) {
+        return;
+      } else {
+        arr[1] = data;
+        heapify(arr, count, 1);
+      }
+    }
+
+    private void heapify(int[] array, int n, int i) {
+      while (true) {
+        int minPos = i;
+        if (i * 2 <= n && array[i] > array[i * 2]) {
+          minPos = i * 2;
+        }
+        if (i * 2 + 1 <= n && array[minPos] > array[i * 2 + 1]) {
+          minPos = i * 2 + 1;
+        }
+        if (minPos == i) {
+          return;
+        }
+        swap(array, i, minPos);
+        i = minPos;
+      }
+    }
+
+    /**
+     * 交换位置
+     */
+    private void swap(int[] array, int i, int j) {
+      int tmp = array[i];
+      array[i] = array[j];
+      array[j] = tmp;
     }
   }
 }
