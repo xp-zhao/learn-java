2583689e3100238bc5bbb337bc4749f6980c886e
xp-zhao <13688396271@163.com>
2019-09-04 10:21:07 +0800

    增加并发模块

--- learn-lambda/src/main/java/com/xp/stream/MaxDemo.java
+++ learn-lambda/src/main/java/com/xp/stream/MaxDemo.java
@@ -15,5 +15,10 @@ public class MaxDemo {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
     System.out.println(numbers.stream().max(Integer::compareTo).get());
     System.out.println(numbers.stream().mapToInt(i -> i).max().getAsInt());
+
+    String name = "\"测试\"";
+    System.out.println(name.replaceAll("\"", ""));
+    System.out.println(name.equals("测试"));
+
   }
 }
