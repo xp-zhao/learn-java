3e0ce97f7a5e19edeb7777bd6399f4ca642a7d2c
xp-zhao <13688396271@163.com>
2019-09-10 17:47:45 +0800

    volatile/LongAdder示例

---
+++ learn-concurrent/src/main/java/concurrent/VolatileNotAtomic.java
@@ -0,0 +1,32 @@
+package concurrent;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * VolatileNotAtomic.java valotile关键字示例
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/09/10
+ **/
+public class VolatileNotAtomic {
+
+  private static final int NUMBER = 10000;
+  private static LongAdder longAdder = new LongAdder();
+
+  public static void main(String[] args) {
+    Thread thread = new Thread(() -> {
+      for (int i = 0; i < NUMBER; i++) {
+        longAdder.decrement();
+      }
+    }, "thread");
+    thread.start();
+    for (int i = 0; i < NUMBER; i++) {
+      longAdder.increment();
+    }
+    while (thread.isAlive()) {
+
+    }
+
+    System.out.println("count 最后的值为：" + longAdder.intValue());
+  }
+}
