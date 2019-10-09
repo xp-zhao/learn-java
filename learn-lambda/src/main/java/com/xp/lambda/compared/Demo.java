2165691adfc15b7b552f86b95a43d1cc1c006829
xp-zhao <13688396271@163.com>
2019-10-09 15:01:36 +0800

    lambda表达式多字段去重

--- learn-lambda/src/main/java/com/xp/lambda/compared/Demo.java
+++ learn-lambda/src/main/java/com/xp/lambda/compared/Demo.java
@@ -1,8 +1,12 @@
 package com.xp.lambda.compared;
 
 import com.xp.lambda.compared.entity.Person;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 /**
  * Demo.java
@@ -15,9 +19,17 @@ public class Demo {
   public static void main(String[] args) {
     List<Person> personList = Arrays
         .asList(Person.builder().firstName("xp").lastName("zhao2").build(),
-            Person.builder().firstName("xp1").lastName("zhao1").build());
+            Person.builder().firstName("xp1").lastName("zhao1").age(20).build(),
+            Person.builder().firstName("xp1").lastName("zhao3").age(20).build());
     System.out.println(personList);
-    personList.stream().forEach(item -> item.setAge(1));
-    System.out.println(personList);
+//    personList.stream().forEach(item -> item.setAge(1));
+//    System.out.println(personList);
+    // 根据 firstName、age 去重
+    List<Person> distinctList = personList.stream()
+        .collect(Collectors.collectingAndThen(Collectors.toCollection(
+            () -> new TreeSet<>(
+                Comparator.comparing(item -> item.getFirstName() + ":" + item.getAge()))),
+            ArrayList::new));
+    System.out.println(distinctList);
   }
 }
