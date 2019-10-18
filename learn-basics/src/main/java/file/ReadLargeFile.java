86ee107faf8f06fc274a4df4b12f7a125e17cf89
xp-zhao <13688396271@163.com>
2019-10-18 14:27:44 +0800

    Java读取大文件

---
+++ learn-basics/src/main/java/file/ReadLargeFile.java
@@ -0,0 +1,63 @@
+package file;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+/**
+ * file.ReadLargeFile.java 读取大文件示例
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/10/18
+ **/
+public class ReadLargeFile {
+
+  public static void main(String[] args) throws IOException {
+    String path = buildFile();
+    scannerRead(path);
+    commonsIoRead(path);
+  }
+
+  public static void scannerRead(String path) {
+    try (FileInputStream inputStream = new FileInputStream(path);
+        Scanner sc = new Scanner(inputStream, "UTF-8")) {
+      int i = 0;
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+        i++;
+      }
+      System.out.println(String.format("总行数 %d 行", i));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void commonsIoRead(String path) {
+    File file = new File(path);
+    try (LineIterator iterator = FileUtils.lineIterator(file, "UTF-8")) {
+      int i = 0;
+      while (iterator.hasNext()) {
+        String line = iterator.nextLine();
+        i++;
+      }
+      System.out.println(String.format("总行数 %d 行", i));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static String buildFile() throws IOException {
+    File file = new File("largeFile.txt");
+    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+    for (int i = 0; i < 10; i++) {
+      writer.write("file" + i + "\n");
+    }
+    writer.close();
+    return file.getAbsolutePath();
+  }
+}
