331f30499d3fa44af6cbc04d8918af8d31b5025d
xp-zhao <13688396271@163.com>
2019-10-18 17:12:35 +0800

    Synchronized关键字使用示例

---
+++ learn-basics/src/main/java/concurrent/SychronizedDemo.java
@@ -0,0 +1,36 @@
+package concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import lombok.Data;
+
+/**
+ * SychronizedDemo.java Synchronized 关键字使用
+ *
+ * @author: zhaoxiaoping
+ * @date: 2019/10/18
+ **/
+public class SychronizedDemo {
+
+  public static void main(String[] args) throws InterruptedException {
+    ExecutorService service = Executors.newFixedThreadPool(3);
+    SynchronizedMethods summation = new SynchronizedMethods();
+
+    IntStream.range(0, 1000).forEach(count -> service.submit(summation::calculate));
+    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+    service.shutdown();
+    System.out.println(summation.getSum());
+  }
+
+  @Data
+  static class SynchronizedMethods {
+
+    private int sum = 0;
+
+    public synchronized void calculate() {
+      setSum(getSum() + 1);
+    }
+  }
+}
