6a1c1c16c3babaefa54fe8f50d13640fd76a84d3
xp-zhao <13688396271@163.com>
2020-08-12 23:42:39 +0800

    wmf转svg

---
+++ learn-test/src/test/java/wmf/WmfConvert.java
@@ -0,0 +1,54 @@
+package wmf;
+
+import net.arnx.wmf2svg.gdi.svg.SvgGdi;
+import net.arnx.wmf2svg.gdi.svg.SvgGdiException;
+import net.arnx.wmf2svg.gdi.wmf.WmfParseException;
+import net.arnx.wmf2svg.gdi.wmf.WmfParser;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+
+public class WmfConvert {
+
+    @Test
+    public void testConvert() throws Exception {
+        String path = "/Users/hejuan/Desktop/1-提取图片_image6.wmf";
+        String dest = "/Users/hejuan/Desktop/1-提取图片_image6.svg";
+        InputStream is = new FileInputStream(path);
+        WmfParser parser = new WmfParser();
+        final SvgGdi gdi = new SvgGdi(false);
+        parser.parse(is, gdi);
+        Document doc = gdi.getDocument();
+        OutputStream out = new FileOutputStream(dest);
+        output(doc, out);
+        if (out != null) {
+            out.close();
+        }
+        if (is != null) {
+            is.close();
+        }
+    }
+
+    private void output(Document doc, OutputStream out) throws Exception {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = factory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC,
+                "-//W3C//DTD SVG 1.0//EN");
+        transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,
+                "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd");
+        transformer.transform(new DOMSource(doc), new StreamResult(out));
+        if (out != null) {
+            out.flush();
+            out.close();
+        }
+    }
+}
