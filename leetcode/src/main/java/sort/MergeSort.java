a3eab8942616668f5f483c7ad8eb89adfbde7973
xp-zhao <13688396271@163.com>
2020-10-13 16:14:49 +0800

    归并排序

--- leetcode/src/main/java/sort/MergeSort.java
+++ leetcode/src/main/java/sort/MergeSort.java
@@ -1,13 +1,57 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
- * 归并排序
- * Created by xp-zhao on 2018/9/13.
+ * 归并排序 Created by xp-zhao on 2018/9/13.
  */
-public class MergeSort
-{
-	public static void main(String[] args)
-	{
-		int[] arr = {5 , 3 ,1, 7 , 4 , 9 , 2};
-	}
+public class MergeSort {
+
+  public static void main(String[] args) {
+    int[] arr = {5, 3, 1, 7, 4, 9, 2};
+    System.out.println(Arrays.toString(mergeSort(arr)));
+  }
+
+  public static int[] mergeSort(int[] arr) {
+    if (arr == null) {
+      return new int[0];
+    }
+    if (arr.length == 1) {
+      return arr;
+    }
+    int mid = arr.length / 2;
+    int[] left = Arrays.copyOfRange(arr, 0, mid);
+    int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return mergeArrays(left, right);
+  }
+
+  public static int[] mergeArrays(int[] a, int[] b) {
+    if (a == null) {
+      return new int[0];
+    }
+    if (b == null) {
+      return new int[0];
+    }
+    int[] merged = new int[a.length + b.length];
+    int ai = 0, bi = 0, mi = 0;
+    while (ai < a.length && bi < b.length) {
+      if (a[ai] < b[bi]) {
+        merged[mi] = a[ai];
+        ai++;
+      } else {
+        merged[mi] = b[bi];
+        bi++;
+      }
+      mi++;
+    }
+    while (ai < a.length) {
+      merged[mi++] = a[ai++];
+    }
+    while (bi < b.length) {
+      merged[mi++] = b[bi++];
+    }
+    return merged;
+  }
 }
