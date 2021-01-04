e108c4b39e810ac8b32f5d8c4e45071905d7e330
xp-zhao <13688396271@163.com>
2021-01-04 23:54:18 +0800

    二叉树打印

--- leetcode/src/main/java/tree/LeetCode_226_InvertBinaryTree.java
+++ leetcode/src/main/java/tree/LeetCode_226_InvertBinaryTree.java
@@ -3,41 +3,48 @@ package tree;
 /**
  * 226. 翻转二叉树
  *
- * Created by xp-zhao on 2019/1/10.
+ * <p>Created by xp-zhao on 2019/1/10.
  */
-public class LeetCode_226_InvertBinaryTree
-{
-	public static void main(String[] args) {
-		TreeNode root = new TreeNode(4);
-		TreeNode rootLeft = new TreeNode(2);
-		TreeNode rootRight = new TreeNode(7);
-		TreeNode rootLeftLeft = new TreeNode(1);
-		TreeNode rootLeftRight = new TreeNode(3);
-		TreeNode rootRightLeft = new TreeNode(6);
-		TreeNode rootRightRight = new TreeNode(9);
-		root.left = rootLeft;
-		root.right = rootRight;
-		rootLeft.left = rootLeftLeft;
-		rootLeft.right = rootLeftRight;
-		rootRight.left = rootRightLeft;
-		rootRight.right = rootRightRight;
-		System.out.println(root);
-		System.out.println(invertTree(root));
-	}
+public class LeetCode_226_InvertBinaryTree {
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(4);
+    TreeNode rootLeft = new TreeNode(2);
+    TreeNode rootRight = new TreeNode(7);
+    TreeNode rootLeftLeft = new TreeNode(1);
+    TreeNode rootLeftRight = new TreeNode(3);
+    TreeNode rootRightLeft = new TreeNode(6);
+    TreeNode rootRightRight = new TreeNode(9);
+    root.left = rootLeft;
+    root.right = rootRight;
+    rootLeft.left = rootLeftLeft;
+    rootLeft.right = rootLeftRight;
+    rootRight.left = rootRightLeft;
+    rootRight.right = rootRightRight;
+    System.out.println(root);
+    System.out.println(count(root));
+    System.out.println(invertTree(root));
+  }
 
-	public static TreeNode invertTree(TreeNode root) {
-		if(root == null){
-			return null;
-		}
-		TreeNode left = root.left;
-		root.left = root.right;
-		root.right = left;
-		if(root.left != null){
-			invertTree(root.left);
-		}
-		if(root.right != null){
-			invertTree(root.right);
-		}
-		return root;
-	}
+  public static TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    TreeNode left = root.left;
+    root.left = root.right;
+    root.right = left;
+    if (root.left != null) {
+      invertTree(root.left);
+    }
+    if (root.right != null) {
+      invertTree(root.right);
+    }
+    return root;
+  }
+
+  public static int count(TreeNode root){
+    if(root == null){
+      return 0;
+    }
+    return 1 + count(root.left) + count(root.right);
+  }
 }
--- leetcode/src/main/java/tree/TreeNode.java
+++ leetcode/src/main/java/tree/TreeNode.java
@@ -2,35 +2,33 @@ package tree;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringJoiner;
 
-/**
- * Created by xp-zhao on 2019/1/10.
- */
-public class TreeNode
-{
-	public int val;
-	public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int x)
-	{
-		val = x;
-	}
+/** Created by xp-zhao on 2019/1/10. */
+public class TreeNode {
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
 
-	@Override
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		Queue<TreeNode> queue = new LinkedList<>();
-		queue.add(this);
-		while(!queue.isEmpty()){
-			TreeNode current = queue.remove();
-			sb.append(current.val+",");
-			if(current.left != null){
-				queue.add(current.left);
-			}
-			if(current.right != null){
-				queue.add(current.right);
-			}
-		}
-		return sb.toString();
-	}
+  public TreeNode(int x) {
+    val = x;
+  }
+
+  @Override
+  public String toString() {
+    StringJoiner joiner = new StringJoiner(",");
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(this);
+    while (!queue.isEmpty()) {
+      TreeNode current = queue.remove();
+      joiner.add(String.valueOf(current.val));
+      if (current.left != null) {
+        queue.add(current.left);
+      }
+      if (current.right != null) {
+        queue.add(current.right);
+      }
+    }
+    return joiner.toString();
+  }
 }
