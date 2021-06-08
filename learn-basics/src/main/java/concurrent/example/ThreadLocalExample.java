e2d3d6d161113c025b27043062602ef8f9f0e793
xp-zhao <13688396271@163.com>
2021-06-08 10:27:35 +0800

    ThreadLocal 常见使用问题

---
+++ learn-basics/src/main/java/concurrent/example/ThreadLocalExample.java
@@ -0,0 +1,42 @@
+package concurrent.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhaoxiaoping
+ * @Description: ThreadLocal 使用示例
+ * @Date 2021-6-8
+ **/
+public class ThreadLocalExample {
+
+  private static ThreadLocal<String> currentUser = ThreadLocal.withInitial(() -> null);
+
+  public static void main(String[] args) throws InterruptedException {
+    ForkJoinPool forkJoinPool = new ForkJoinPool(1);
+    ThreadLocalExample example = new ThreadLocalExample();
+    forkJoinPool.execute(() -> {
+      example.print();
+      example.print();
+    });
+    forkJoinPool.shutdown();
+    forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+  }
+
+  private void print() {
+    String before = Thread.currentThread().getName() + ":" + currentUser.get();
+    currentUser.set("1");
+    try {
+      String after = Thread.currentThread().getName() + ":" + currentUser.get();
+      Map<String, String> result = new HashMap<>(2);
+      result.put("before", before);
+      result.put("after", after);
+      System.out.println(result);
+    } finally {
+      // 在使用结束之后删除 ThreadLocal 中的数据, 防止线程重用导致 ThreadLocal 信息错乱
+      currentUser.remove();
+    }
+  }
+}
