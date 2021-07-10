4a49321a454dcac6d5fff8c8e5ff9ccd3ef5f31f
xp-zhao <13688396271@163.com>
2021-07-10 17:56:55 +0800

    增加对象属性

---
+++ learn-spring/small-spring/src/main/java/org/smallspring/beans/factory/config/PropertyValue.java
@@ -0,0 +1,25 @@
+package org.smallspring.beans.factory.config;
+
+/**
+ * @author zhaoxiaoping
+ * @Description:
+ * @Date 2021-7-10
+ **/
+public class PropertyValue {
+
+  private final String name;
+  private final String value;
+
+  public PropertyValue(String name, String value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
---
+++ learn-spring/small-spring/src/main/java/org/smallspring/beans/factory/config/PropertyValues.java
@@ -0,0 +1,31 @@
+package org.smallspring.beans.factory.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhaoxiaoping
+ * @Description:
+ * @Date 2021-7-10
+ **/
+public class PropertyValues {
+
+  private final List<PropertyValue> propertyValueList = new ArrayList<>();
+
+  public void addPropertyValue(PropertyValue pv) {
+    this.propertyValueList.add(pv);
+  }
+
+  public PropertyValue[] getPropertyValues() {
+    return this.propertyValueList.toArray(new PropertyValue[0]);
+  }
+
+  public PropertyValue getPropertyValue(String propertyName) {
+    for (PropertyValue propertyValue : this.propertyValueList) {
+      if (propertyValue.getName().equals(propertyName)) {
+        return propertyValue;
+      }
+    }
+    return null;
+  }
+}
