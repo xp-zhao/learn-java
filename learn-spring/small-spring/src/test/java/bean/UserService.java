10ef0b86849ff040c66cf23d048f61258f5b72ff
xp-zhao <13688396271@163.com>
2021-08-18 19:57:36 +0800

    small-spring UserService修改

--- learn-spring/small-spring/src/test/java/bean/UserService.java
+++ learn-spring/small-spring/src/test/java/bean/UserService.java
@@ -2,16 +2,18 @@ package bean;
 
 import cn.hutool.core.util.StrUtil;
 
-/**
- * @Author: xp-zhao @Description: TODO @DateTime: 2021/7/7 11:19 下午
- */
+/** @Author: xp-zhao @Description: TODO @DateTime: 2021/7/7 11:19 下午 */
 public class UserService {
 
   private String id;
+  private String uId;
+  private String company;
+  private String location;
+
   private UserDao userDao;
 
   public void queryUserInfo() {
-    System.out.println(StrUtil.format("查询用户信息: {}", userDao.queryUserName(id)));
+    System.out.println(StrUtil.format("查询用户信息: {}", userDao.queryUserName(uId)));
   }
 
   public String getId() {
