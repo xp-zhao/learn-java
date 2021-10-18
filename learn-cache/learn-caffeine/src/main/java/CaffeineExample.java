403ca3a7266046c1038ba426b5187628988412cd
xp-zhao <13688396271@163.com>
2021-10-18 09:40:53 +0800

    Caffeine 使用示例

--- learn-cache/learn-caffeine/src/main/java/CaffeineExample.java
+++ learn-cache/learn-caffeine/src/main/java/CaffeineExample.java
@@ -12,12 +12,20 @@ public class CaffeineExample {
             .maximumSize(100)
             .expireAfterWrite(1, TimeUnit.DAYS)
             .build();
+    Cache<String, Integer> cache1 =
+        Caffeine.newBuilder()
+            .initialCapacity(10)
+            .maximumSize(100)
+            .expireAfterWrite(1, TimeUnit.DAYS)
+            .build();
     System.out.println(cache.get("key", CaffeineExample::getValueForDb));
     System.out.println(cache.get("key", CaffeineExample::getValueForDb));
+    System.out.println(cache1.get("key1", key -> getValueForDb(key).orElse(null)));
+    System.out.println(cache1.get("key1", key -> getValueForDb(key).orElse(null)));
   }
 
   public static Optional<Integer> getValueForDb(String key) {
-    System.out.println("缓存中未查询到数据, 从数据库中读取");
+    System.out.println(key + " 缓存中未查询到数据, 从数据库中读取");
     return Optional.empty();
   }
 }
