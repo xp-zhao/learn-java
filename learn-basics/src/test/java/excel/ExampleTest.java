519b705c8f3879962493995451fb7ff42d0a7963
xp-zhao <13688396271@163.com>
2022-02-28 17:15:00 +0800

    CountDownLatch 使用场景示例

---
+++ learn-basics/src/test/java/excel/ExampleTest.java
@@ -0,0 +1,59 @@
+package excel;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author zhaoxiaoping
+ * @date 2022-2-28
+ */
+public class ExampleTest {
+
+  private static int x = 0, y = 0;
+  private static int a = 0, b = 0;
+
+  public static void main(String[] args) throws InterruptedException {
+    int i = 0;
+    for (; ; ) {
+      i++;
+      x = 0;
+      y = 0;
+      a = 0;
+      b = 0;
+      CountDownLatch latch = new CountDownLatch(1);
+      Thread one =
+          new Thread(
+              () -> {
+                try {
+                  latch.await();
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                a = 1;
+                x = b;
+              });
+      Thread other =
+          new Thread(
+              () -> {
+                try {
+                  latch.await();
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                b = 1;
+                y = a;
+              });
+      one.start();
+      other.start();
+      latch.countDown();
+      one.join();
+      other.join();
+      String result = "第" + i + "次 (" + x + "," + y + "）";
+      if (x == 0 && y == 0) {
+        System.err.println(result);
+        break;
+      } else {
+        System.out.println(result);
+      }
+    }
+  }
+}
--- learn-test/src/main/java/com/xp/thread/juc/CountDownLatchDemo.java
+++ learn-test/src/main/java/com/xp/thread/juc/CountDownLatchDemo.java
@@ -1,29 +1,76 @@
 package com.xp.thread.juc;
 
+import cn.hutool.core.util.StrUtil;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
-/**
- * CountDownLatch 允许一个或者多个线程等待其他线程完成操作
- */
+/** CountDownLatch 允许一个或者多个线程等待其他线程完成操作 */
 public class CountDownLatchDemo {
-    public static void main(String[] args) throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-        new Thread(() -> {
-            System.out.println(1);
-            countDownLatch.countDown();
-            System.out.println(2);
-            countDownLatch.countDown();
-        }).start();
-        countDownLatch.await(); // 阻塞当前线程，直到 countDownLatch.countDown 减到 0
-        System.out.println(3);
-    }
+  public static void main(String[] args) throws InterruptedException {
+    //    test();
+//    firstScene();
+      secondScene();
+  }
 
-    public static void sleep(long time){
-        try {
-            TimeUnit.MICROSECONDS.sleep(time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+  private static void test() throws InterruptedException {
+    CountDownLatch countDownLatch = new CountDownLatch(2);
+    new Thread(
+            () -> {
+              System.out.println(1);
+              countDownLatch.countDown();
+              System.out.println(2);
+              countDownLatch.countDown();
+            })
+        .start();
+    countDownLatch.await(); // 阻塞当前线程，直到 countDownLatch.countDown 减到 0
+    System.out.println(3);
+  }
+
+  /** 场景一，让多个线程等待 */
+  public static void firstScene() {
+    CountDownLatch latch = new CountDownLatch(1);
+    for (int i = 0; i < 5; i++) {
+      new Thread(
+              () -> {
+                try {
+                  // 所有线程都阻塞在这儿，等待计数器为 0
+                  latch.await();
+                  System.out.println(
+                      StrUtil.format("[{}] 开始执行...", Thread.currentThread().getName()));
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              })
+          .start();
     }
+    // 等待 2s 后将计数器减一
+    sleep(2000);
+    latch.countDown();
+  }
+
+  /** 场景二，让单个线程等待 */
+  public static void secondScene() throws InterruptedException {
+    CountDownLatch latch = new CountDownLatch(5);
+    for (int i = 0; i < 5; i++) {
+      new Thread(
+              () -> {
+                sleep(ThreadLocalRandom.current().nextInt(1000));
+                System.out.println(Thread.currentThread().getName());
+                latch.countDown();
+              })
+          .start();
+    }
+    // 主线程阻塞，等待计数器为 0
+    latch.await();
+    System.out.println("所有线程执行结束");
+  }
+
+  public static void sleep(long time) {
+    try {
+      TimeUnit.MILLISECONDS.sleep(time);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
