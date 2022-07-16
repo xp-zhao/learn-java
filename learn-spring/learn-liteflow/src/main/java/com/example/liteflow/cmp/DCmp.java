0b89d97e727baa7e82db75c4c7b25894a2b559e4
xp-zhao <13688396271@163.com>
2022-07-16 11:04:33 +0800

    liteflow私有投递示例补充

--- learn-spring/learn-liteflow/src/main/java/com/example/liteflow/cmp/DCmp.java
+++ learn-spring/learn-liteflow/src/main/java/com/example/liteflow/cmp/DCmp.java
@@ -4,6 +4,8 @@ import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 /**
  * TODO
  *
@@ -16,8 +18,17 @@ public class DCmp extends NodeComponent {
   @Override
   public void process() throws Exception {
     log.info("前置模块执行完成");
-    for (int i = 0; i < 5; i++) {
-      this.sendPrivateDeliveryData("a", 2);
+    int total = 17;
+    int aCount = 5;
+    int[] arr = new int[aCount];
+    int remainderCount = total % aCount;
+    int divideCount = total / aCount;
+    Arrays.fill(arr, divideCount);
+    for (int i = 0; i < remainderCount; i++) {
+      arr[i] += 1;
+    }
+    for (int i : arr) {
+      this.sendPrivateDeliveryData("a", i);
     }
   }
 }
