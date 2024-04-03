a321e729b7539bb02d0ba5e9a36565a0f6aa9bff
xp-zhao <13688396271@163.com>
2024-04-03 18:02:25 +0800

    dockerfile 示例

---
+++ learn-design/src/main/java/com/DockerfileTest.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author zhaoxiaoping
+ * @date 2024-4-3
+ */
+public class DockerfileTest {
+  public static void main(String[] args) {
+    System.out.println("hell world");
+  }
+}
