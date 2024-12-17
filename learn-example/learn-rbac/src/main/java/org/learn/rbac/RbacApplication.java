fbbc062615731478ba42db36b4a304ea486d86a2
xp-zhao <13688396271@163.com>
2024-12-17 22:27:55 +0800

    新增 learn-rbac 模块

---
+++ learn-example/learn-rbac/src/main/java/org/learn/rbac/RbacApplication.java
@@ -0,0 +1,16 @@
+package org.learn.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xp-zhao
+ * @description
+ * @date 2024/12/17 20:30
+ */
+@SpringBootApplication
+public class RbacApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RbacApplication.class, args);
+  }
+}
---
+++ learn-example/learn-rbac/src/main/resources/db/hsqldb/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES (1, 'a');
+INSERT INTO users VALUES (2, 'b');
+INSERT INTO users VALUES (3, 'c');
